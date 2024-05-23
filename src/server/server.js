b8c5114a224f0e354b4019cb2cad8096c1e71718
Jose Shardin <joseruiz.sh@gmail.com>
2024-05-23 17:59:49 -0500

    realizamos un servidorweb donde servimos una web de tipo RRSS tipo X

---
+++ src/server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+
+const startServer = (options) => {
+  const { port, public_path = 'public' } = options
+  // console.log(port)
+  // console.log(public_path)
+
+  const app = express()
+
+  //middleware
+  app.use(express.static(public_path))
+
+  app.get('*', (req, res) => {
+    const indexPath = path.join(__dirname + `../../${public_path}/index.html`)
+    res.sendFile(indexPath)
+  })
+
+  app.listen(port, () => {
+    console.log(`escuchando en el puerto ${port}`)
+  })
+}
+
+module.exports = {
+  startServer
+}
